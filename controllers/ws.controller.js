4c51d62e6fc8aa09c7f58f89315e4882cae7d02c
ridweng <i93kaiser@hotmail.com>
2022-04-12 13:37:10 +0100

    Message changed

--- controllers/ws.controller.js
+++ controllers/ws.controller.js
@@ -2,24 +2,25 @@ const fs = require('fs')
 
 exports.ws =  (ws ,req) => {
     try{
-        const mess = `connection from: ${req.params.id} in ${req._remoteAddress} at ${req._startTime}.\n`
+        const mess = `connection from: ${req.params.id} in ${req._remoteAddress} at ${req._startTime}.`
+        const line = '\n'
         const initialMess =`Started ${mess}`
         const file = './access.log'
         console.log(initialMess)
         let writer = fs.createWriteStream(file, { flags: 'a' }) 
-        writer.write(initialMess);
+        writer.write(initialMess + line);
 
         ws.send('Connected at: ' + new Date())
         
         ws.on('message', function incoming(message) {
-            console.log('received: %s', message);
+            console.log(`${req.params.id} said: ${message}`);
             ws.send('message from client at: ' + new Date());
         });
     
         ws.on('close', () => {
             const finalMess = `Stopped ${mess}`
             let writer = fs.createWriteStream(file, { flags: 'a' }) 
-            writer.write(finalMess);
+            writer.write(finalMess + line);
             console.log(finalMess)
         })
     }catch(err){
